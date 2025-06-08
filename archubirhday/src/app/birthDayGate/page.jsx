e503a302b311ba951f696a3465921fd9c8a4fadc
Priyabrata9861 <ptiyabratamishra2001@gmail.com>
2025-06-08 12:53:22 +0530

    changes

--- archubirhday/src/app/birthDayGate/page.jsx
+++ archubirhday/src/app/birthDayGate/page.jsx
@@ -1,57 +1,24 @@
-// 'use client';
-
-// import React from 'react';
-// import './BirthdayGate.css'; 
-// import { Container } from 'react-bootstrap';
-
-// function BirthdayGate() {
-//   return (
-//     <Container>
-//     <div className="birthday-gate-wrapper">
-//       <div></div>
-//       <div className="video-card">
-//         <video
-//           className="birthday-video"
-//           autoPlay
-//           controls
-//           muted
-//         >
-//           <source src="/archanaBirthDay/birthDay.mp4" type="video/mp4" />
-//           Your browser does not support the video tag.
-//         </video>
-//       </div>
-//       <div></div>
-//     </div>
-//     </Container>
-//   );
-// }
-
-// export default BirthdayGate;
-
 'use client';
-
 import React from 'react';
-import './BirthdayGate.css'; 
-import { Container } from 'react-bootstrap';
+import './BirthdayGate.css';
 
 function BirthdayGate() {
   return (
-    <Container>
-      <div className="birthday-gate-wrapper">
-        <div className="sparkle-container left-sparkles"></div>
+    <div className="birthday-gate-wrapper">
+      <h1 className="birthday-heading">🎉 Happy Birthday Archana 🎉</h1>
 
-        <div className="video-card">
-          <video className="birthday-video" autoPlay controls muted>
-            <source src="/archanaBirthDay/birthDay.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
+      <div className="sparkle-container left-sparkles"></div>
 
-        <div className="sparkle-container right-sparkles"></div>
+      <div className="video-card">
+        <video className="birthday-video" autoPlay controls muted>
+          <source src="/archanaBirthDay/birthDay.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-    </Container>
+
+      <div className="sparkle-container right-sparkles"></div>
+    </div>
   );
 }
 
 export default BirthdayGate;
-
