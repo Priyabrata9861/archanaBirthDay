793051179014648f6b253add67dd35ea5137c0e8
Priyabrta <priyabratamishra2001@gmail.com>
2026-03-22 14:37:47 +0530

    Add birthday icon and new image for celebration page

--- archubirhday/src/app/layout.js
+++ archubirhday/src/app/layout.js
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Isha Birthday",
   description: "A special birthday celebration page 🎉",
+   icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎂</text></svg>",
+  },
 };
 
 export default function RootLayout({ children }) {
